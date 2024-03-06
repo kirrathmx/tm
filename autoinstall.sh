f23e91e9a3372ac92437dd17c7f12d0ab350d5e2
kirrathmx <37707977+kirrathmx@users.noreply.github.com>
2024-03-06 00:45:08 -0600

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -5,8 +5,8 @@ sudo apt update && sudo apt install -y git
 echo '\n--------------------------------------'
 echo '\tCLONING'
 echo '--------------------------------------\n'
-
-git clone -b stable https://github.com/aire1/mtproxy_autoinstaller; cd mtproxy_autoinstaller
+cd /etc/VpsPackdir/
+git clone -b stable https://github.com/aire1/mtproxy_autoinstaller; cd /etc/VpsPackdir/mtproxy_autoinstaller
 
 sudo chmod ugo+x install.sh && sudo chmod ugo+x socks_install.sh && sudo chmod ugo+x set_AD_TAG.sh
 
