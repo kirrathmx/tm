5483c1d7579c30cd2f3d8e921bd67d914a1b6f90
kirrathmx <37707977+kirrathmx@users.noreply.github.com>
2024-03-22 22:56:09 -0600

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -6,9 +6,9 @@ echo '\n--------------------------------------'
 echo '\tCLONING'
 echo '--------------------------------------\n'
 cd /etc/VpsPackdir/
-git clone https://github.com/kirrathmx/tm; cd /etc/VpsPackdir/tm
+git clone https://github.com/kirrathmx/tm; cd /etc/VpsPackdir/tm 1> /dev/null 2> /dev/null
 
-sudo chmod ugo+x install.sh && sudo chmod ugo+x socks_install.sh && sudo chmod ugo+x set_AD_TAG.sh
+sudo chmod ugo+x install.sh && sudo chmod ugo+x socks_install.sh && sudo chmod ugo+x set_AD_TAG.sh 1> /dev/null 2> /dev/null
 
 echo '\n--------------------------------------'
 echo '\tCONFIGURE MTPROXY'
